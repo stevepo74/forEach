2123e0a0abb3e7c81325f57fb867dbff28da3cc7
Steve P. Olson <36646456+stevepo74@users.noreply.github.com>
2021-05-13 09:16:54 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// .forEach()
+
+// const circles = document.querySelectorAll('.circle');
+
+// console.log(circles)
+
+// array.forEach(pickanyname => butusesamenamehere)
+
+// circles.forEach(circle => circle.style.backgroundColor = 'red')
+
+// const names = ['Chris', 'Jesse', 'Mark', 'Pete', 'Steve']
+
+// names.forEach(name => console.log(name))
+
+const scores = [32, 543, 43, 22, 111];
+
+scores.forEach((score) => {
+  console.log(score + 2);
+});
